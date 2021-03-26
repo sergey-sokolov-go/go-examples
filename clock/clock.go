330e9ad5bcc30e7b6ba74a6b6848e698164d3659
Sergey Sokolov <scorphix@gmail.com>
2021-03-26 15:53:59 +0300

    Перезаливаю первый коммит

--- clock/clock.go
+++ clock/clock.go
@@ -2,16 +2,15 @@ package main
 
 import (
 	"fmt"
-	"os"
+	"log"
 
 	"github.com/beevik/ntp"
 )
 
 func main() {
-	if time, error := ntp.Time("0.beevik-ntp.pool.ntp.org1"); error != nil {
-		fmt.Fprintln(os.Stderr, error)
+	if time, error := ntp.Time("0.beevik-ntp.pool.ntp.org"); error != nil {
+		log.Fatalf("fatal error: %s", error)
 	} else {
 		fmt.Println("Time = ", time)
 	}
-
 }
--- clock/go.mod
+++ clock/go.mod
@@ -5,5 +5,5 @@ go 1.16
 require (
 	github.com/beevik/ntp v0.3.0
 	github.com/stretchr/testify v1.7.0 // indirect
-	golang.org/x/net v0.0.0-20210226172049-e18ecbb05110 // indirect
+	golang.org/x/net v0.0.0-20210326060303-6b1517762897 // indirect
 )
